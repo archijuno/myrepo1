3fb43ddb2122a2274fe02cde75c976b94a40bec4
archijuno <129946937+archijuno@users.noreply.github.com>
2023-04-20 23:45:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello,I'm JUNO!!! Nice to meet u')
+  res.send('Hello,I am JUNO!!! Nice to meet u')
 })
 
 app.listen(4000, ()=>{
